29625253a08c506fc1b1053194c58d45a3ba3d76
kyhyy <61657745+kyhyy@users.noreply.github.com>
2026-07-18 01:00:49 +0200

    Add subcommand dispatch skeleton to CLI entry point

--- cmd/roomux/main.go
+++ cmd/roomux/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("roomux: oh hi Mark")
+	if len(os.Args) < 2 {
+		usage()
+		return
+	}
+
+	switch os.Args[1] {
+	default:
+		fmt.Fprintf(os.Stderr, "roomux: unknown command %q\n\n", os.Args[1])
+		usage()
+		os.Exit(1)
+	}
+}
+
+func usage() {
+	fmt.Println("usage: roomux <command> [args]")
+	fmt.Println()
+	fmt.Println("commands:")
+	fmt.Println("  (none yet)")
 }
